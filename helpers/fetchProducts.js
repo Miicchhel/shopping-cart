3e915a72498e9fa0034b077dd6d43cecd9ce0d2d
Michel Ferreira <michel.ferreira.melo@gmail.com>
2022-08-09 21:05:25 -0300

    Desenvolvendo testes de no mínimo 25% de cobertura total e 100% da função fetchProducts

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,6 +1,24 @@
-const fetchProducts = () => {
+// const fetch = require('node-fetch');
+
+const fetchProducts = async (elemento) => {
   // seu código aqui
+  if (!elemento) {
+    // console.log('entrou');
+    throw new Error('You must provide an url');
+  }
+  const url = `https://api.mercadolibre.com/sites/MLB/search?q=${elemento}`;
+  const request = await fetch(url);
+  const json = await request.json();
+  // console.log(json);
+  return json;
 };
+
+// fetchProducts();
+
+// const fetchProducts = (QUERY) => fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${QUERY}`)
+// .then((response) => response.json())
+// .then((data) => data.results)
+// .catch((error) => error); 
 
 if (typeof module !== 'undefined') {
   module.exports = {
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -5,7 +5,29 @@ const computadorSearch = require('../mocks/search');
 describe('1 - Teste a função fetchProducts', () => {
   // implemente seus testes aqui
   // fail('Teste vazio');
-  it('vericando se fetchProducts é uma função', () => {
-    
+  it('verificando se fetchProducts é uma função', () => {
+    expect(typeof fetchProducts).toEqual('function');
+  });
+
+  it('verificando se "fetch" é chamado ao executar fetchProducts("computador")', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
+  });
+
+  it('verificando se fetch usa a URL correta ao executar fetchProducts("computador")', async () => {
+    await fetchProducts('computador');
+    const URL = `https://api.mercadolibre.com/sites/MLB/search?q=computador`;
+    expect(fetch).toHaveBeenCalledWith(URL);
+  });
+
+  it('verificando se fetchProducts("computador") retorna um objeto igual ao objto computadorSearch', async () => {
+    const esperado = await fetchProducts('computador');
+    const resultado = computadorSearch;
+    expect(esperado).toEqual(resultado);
+  });
+
+  it('verificando se fetchProducts() retorna um erro com a msg: "You must provide an url"', async () => {
+    const resultado = 'You must provide an url';
+    await expect(fetchProducts()).rejects.toThrow(resultado);
   });
 });
